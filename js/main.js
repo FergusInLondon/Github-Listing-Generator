bcfd55d4429ea6ebb5968c0223f660b8ea2881ad
Fergus Morrow <fergus@fergus-morrow.com>
2015-02-25 03:23:20 +0000

    Update index

--- js/main.js
+++ js/main.js
@@ -1,21 +1,44 @@
-			jQuery(
-				function(){
-					jQuery('.link-icons .fa').randomClasses({
-						classes : ['red', 'blue', 'green', 'yellow', 'pink']
-					});
+jQuery(function(){
+	/*/
+	 * Randomise the colours
+	/*/
+	jQuery('.link-icons .fa').randomClasses({
+		classes : ['red', 'blue', 'green', 'yellow', 'pink']
+	});
 
-					// @TODO - actually make this kinda nice.
-					jQuery('[data-toggler]').click(
-						function(){
-							var val = jQuery(this).attr('data-toggler');
-							jQuery('[data-togglee]').removeClass('active');
-							jQuery('[data-toggle="*'+val+'*"]').addClass('active');							
-						}
-					);
+	/*/
+	 * Filter Repositories by language
+	/*/
+	jQuery('[data-toggler]').click(
+		function(){
+			var val = jQuery(this).attr('data-toggler');
+			jQuery('[data-togglee]').removeClass('active');
+			jQuery('[data-toggle="*'+val+'*"]').addClass('active');							
+		}
+	);
 
-					// Fade in the View Repo CTAs - because we like pointless prettiness like that.
-					jQuery('.view-repo').each(function(i){
-						jQuery(this).delay(i*500).fadeIn(250);
-					});
-				}
-			);
+	/*/
+	 * Make the repository icons fade in, individually
+	/*/
+	jQuery('.view-repo').each(function(i){
+		jQuery(this).delay(i*500).fadeIn(250);
+	});
+
+	/*/
+	 * Send the Event to Google Analytics
+	/*/
+	$('repo-link').click(function (e){
+		e.preventDefault();
+		
+		var repo = '',
+			url = $(this).attr('href');
+		
+		ga('send', 'event', 'repositoryView', 'click', repo, 
+			{
+				'hitCallback' : function(){
+					document.location = url;
+				}	
+			}
+		);
+	});
+});
